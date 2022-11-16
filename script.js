c7cd4d892fbcbcd3e4223a968a38eba77d132698
kode kamu <arbikustia8@gmail.com>
2022-11-16 17:05:09 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let bio = document.getElementById("bio");
+let btn = document.getElementById("btn-about")
+function btnAbout() {
+    bio.classList.add("bio-active")
+    btn.style.display = "none"
+}
